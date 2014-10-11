0159c5db97efce1a216538aab4b412c7a7dbaf57
Scott Jordan <sleepinghippy@hotmail.com>
2014-10-10 23:30:15 -0400

    Added next move generation, terminal test, helper functions for looking up counts of resources/buildings

--- captain.rb
+++ captain.rb
@@ -1,28 +1,51 @@
 require 'yaml'
 
+$goods = [:corn, :indigo, :sugar, :tobacco, :coffee]
 class Player
-  def initialize(name, corn=0, indigo=0, sugar=0, tobacco=0, coffee=0, vp=0, wharfs=0, harbor=0)
+  def initialize(name, corn=0, indigo=0, sugar=0, tobacco=0, coffee=0, vp=0, wharfs=0, harbors=0)
     @name = name
     @corn = corn
     @indigo = indigo
     @sugar = sugar
-    @tobacco = coffee
+    @tobacco = tobacco
     @coffee = coffee
     @vp = vp
     @wharfs = wharfs
+    @harbors = harbors
   end
 
-  attr_accessor :id, :corn, :indigo, :sugar, :tobacco, :coffee, :vp, :wharfs, :harbor
+  # returns whether the player has "what", where "what" is
+  # :none, :any, or a symbol of any instance variable's name
+  # :none => no goods
+  # :any => some type of goods
+  def has(what)
+    case what
+    when :none
+      $goods.all? {|x| self.how_many(x) == 0}
+    when :any
+      $goods.any? {|x| self.how_many(x) > 0}
+    else
+      self.how_many(what) > 0
+    end
+  end
+
+  # helper function, looks up how many there are of the named resource
+  # e.g. player.how_many :corn
+  def how_many(what)
+    self.method(what).call
+  end
+
+  attr_accessor :id, :corn, :indigo, :sugar, :tobacco, :coffee, :vp, :wharfs, :harbors
 end
 
 class Ship
-  def initialize(capacity)
+  def initialize(capacity, load=0, type=:none)
     @capacity = capacity
-    @load = 0
-    @type = :none
+    @load = load
+    @type = type
   end
 
-  attr_reader :capacity, :load, :type
+  attr_accessor :capacity, :load, :type
 end
 
 class State
@@ -42,6 +65,32 @@ class State
     nextState = self.clone
     nextState.current_ix = (nextState.current_ix + 1) % @players.size
     return nextState
+  end
+
+  # Retrieve actions that the current player can take.
+  def get_actions(current_player_ix=@current_ix)
+    player = @players[current_player_ix]
+    free_types = ($goods - @ships.map {|s| s.type})
+    @ships.each_with_index.flat_map do |ship,ship_ix|
+      if ship.type == :none
+        types = free_types.select {|g| player.has g}
+        types.map do |g|
+          {:type => g,
+           :ship => ship_ix,
+           :amount => [player.how_many(g), ship.capacity].min}
+        end
+      elsif ship.load < ship.capacity && (player.has ship.type)
+        [{:type => ship.type,
+          :ship => ship_ix,
+          :amount => [player.how_many(ship.type), ship.capacity-ship.load].min}]
+      else
+        []
+      end
+    end
+  end
+  # Whether any actions can be made at this point or not
+  def terminal?
+    (0..@players.length-1).any? {|ix| get_actions(ix).empty?}
   end
 
   attr_reader :players, :ships
--- test.rb
+++ test.rb
@@ -6,6 +6,35 @@ class StateTest < MiniTest::Unit::TestCase
     @players = [Player.new("Scott"), Player.new("Hudson"), Player.new("Chase")]
     @ships = [Ship.new(4), Ship.new(5), Ship.new(6)]
     @state = State.new(@players, @ships)
+
+    @players2 = [Player.new("Scott", 3, 0, 0, 1, 2),
+                 Player.new("Chase", 0, 0, 6, 2, 2),
+                 Player.new("Brian", 8, 4, 0, 0, 0)]
+    @ships2 = [Ship.new(4), Ship.new(5), Ship.new(6, 2, :corn)]
+    @state2 = State.new(@players2, @ships2)
+
+    @ships3 = [Ship.new(4,4,:corn), Ship.new(5,5, :tobacco), Ship.new(6,6,:indigo)]
+    @state3 = State.new(@players2, @ships3)
+
+  end
+
+  def test_has
+    scott = @players[0]
+    assert scott.has :none
+    assert !(scott.has :any)
+    assert !(scott.has :vp)
+
+    chase = @players2[1]
+    assert chase.has :any
+    assert !(chase.has :none)
+    assert chase.has :sugar
+    assert !(chase.has :harbors)
+  end
+
+  def test_how_many
+    chase = @players2[1]
+    assert_equal (chase.how_many :sugar), 6
+    assert_equal (chase.how_many :vp), 0
   end
 
   def test_current_player
@@ -17,5 +46,52 @@ class StateTest < MiniTest::Unit::TestCase
     assert_equal @players[2], state3.current_player
     state4 = state3.act :none
     assert_equal @players[0], state4.current_player
+  end
+
+  def test_get_actions_no_goods
+    # Players with no goods of course cannot take actions
+    (0..2).each do |x|
+      assert_empty @state.get_actions(x)
+    end
+  end
+
+  def test_get_actions
+    expected0 = [{:type => :coffee,  :ship => 0, :amount => 2},
+                 {:type => :coffee,  :ship => 1, :amount => 2},
+                 {:type => :tobacco, :ship => 0, :amount => 1},
+                 {:type => :tobacco, :ship => 1, :amount => 1},
+                 {:type => :corn,    :ship => 2, :amount => 3}]
+
+    expected1 = [{:type => :sugar, :ship => 0, :amount => 4},
+                 {:type => :sugar, :ship => 1, :amount => 5},
+                 {:type => :tobacco, :ship => 0, :amount => 2},
+                 {:type => :tobacco, :ship => 1, :amount => 2},
+                 {:type => :coffee, :ship => 0, :amount => 2},
+                 {:type => :coffee, :ship => 1, :amount => 2}]
+
+    expected2 = [{:type => :indigo, :ship => 0, :amount => 4},
+                 {:type => :indigo, :ship => 1, :amount => 4},
+                 {:type => :corn, :ship => 2, :amount => 4}]
+
+    actual0 = @state2.get_actions(0)
+    actual1 = @state2.get_actions(1)
+    actual2 = @state2.get_actions(2)
+
+    assert expected0 - actual0 == [] && actual0 - expected0 == [], actual0
+    assert expected1 - actual1 == [] && actual1 - expected1 == [], actual1
+    assert expected2 - actual2 == [] && actual2 - expected2 == [], actual2
+  end
+
+  def test_get_actions_full_ships
+    # If ships are full no players can take actions (excepting wharfs)
+    (0..2).each do |x|
+      assert_empty @state3.get_actions(x)
+    end
+  end
+
+  def test_terminal?
+    assert @state.terminal?
+    assert !@state2.terminal?
+    assert @state3.terminal?
   end
 end
